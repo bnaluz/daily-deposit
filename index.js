1ecfeac43a8f9a8ae70b3456db4e80b3023dbdbb
Bobby Naluz <bnaluz9@gmail.com>
2024-08-25 00:48:41 -0400

    day6

--- index.js
+++ index.js
@@ -152,27 +152,75 @@ Problem Statement:
 Write a JavaScript function longestUniqueSubarray that takes an array of integers as input and returns the length of the longest contiguous subarray that contains only unique integers.
 */
 
-function longestUniqueSubarray(nums) {
-  let maxLength = 0;
-  let right = 0;
-  let left = 0;
-  let charSet = new Set();
+// function longestUniqueSubarray(nums) {
+//   let maxLength = 0;
+//   let right = 0;
+//   let left = 0;
+//   let charSet = new Set();
 
-  while (right < nums.length) {
-    if (!charSet.has(nums[right])) {
-      charSet.add(nums[right]);
-      maxLength = Math.max(maxLength, right - left + 1);
-      right++;
-    } else {
-      charSet.delete(nums[left]);
-      left++;
-    }
-  }
-  return maxLength;
+//   while (right < nums.length) {
+//     if (!charSet.has(nums[right])) {
+//       charSet.add(nums[right]);
+//       maxLength = Math.max(maxLength, right - left + 1);
+//       right++;
+//     } else {
+//       charSet.delete(nums[left]);
+//       left++;
+//     }
+//   }
+//   return maxLength;
+// }
+
+// console.log(longestUniqueSubarray([2, 1, 2, 3, 4, 1, 2, 3])); // Expected output: 4
+// console.log(longestUniqueSubarray([1, 2, 3, 4, 5])); // Expected output: 5
+// console.log(longestUniqueSubarray([5, 5, 5, 5, 5])); // Expected output: 1
+// console.log(longestUniqueSubarray([2, 3, 4, 5, 2, 3, 4, 5])); // Expected output: 4
+// console.log(longestUniqueSubarray([1, 2, 3, 1, 2, 3, 4, 5])); // Expected output: 5
+
+//*DAY 6
+
+/*
+*Day 6
+Word Problem:
+You are given an array of integers representing daily temperatures. Your task is to calculate, for each day in the array, how many days you have to wait until a warmer temperature. If there is no future day with a warmer temperature, return 0 for that day.
+
+Problem Statement:
+Write a function dailyTemperatures in JavaScript that takes an array of integers temperatures as input and returns an array of integers representing the number of days to wait until a warmer temperature.
+
+Function Signature:
+function dailyTemperatures(temperatures) {
+  // Your code here
 }
 
-console.log(longestUniqueSubarray([2, 1, 2, 3, 4, 1, 2, 3])); // Expected output: 4
-console.log(longestUniqueSubarray([1, 2, 3, 4, 5])); // Expected output: 5
-console.log(longestUniqueSubarray([5, 5, 5, 5, 5])); // Expected output: 1
-console.log(longestUniqueSubarray([2, 3, 4, 5, 2, 3, 4, 5])); // Expected output: 4
-console.log(longestUniqueSubarray([1, 2, 3, 1, 2, 3, 4, 5])); // Expected output: 5
+Example:
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // Expected output: [1, 1, 4, 2, 1, 1, 0, 0]
+console.log(dailyTemperatures([30, 40, 50, 60])); // Expected output: [1, 1, 1, 0]
+console.log(dailyTemperatures([30, 60, 90])); // Expected output: [1, 1, 0]
+
+Solution:
+*/
+
+function dailyTemperatures(temperatures) {
+  let result = new Array(temperatures.length).fill(0);
+  let stack = [];
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      let j = stack.pop();
+      result[j] = i - j;
+    }
+    stack.push(i);
+  }
+
+  return result;
+}
+
+// Test Cases
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // Expected output: [1, 1, 4, 2, 1, 1, 0, 0]
+console.log(dailyTemperatures([30, 40, 50, 60])); // Expected output: [1, 1, 1, 0]
+console.log(dailyTemperatures([30, 60, 90])); // Expected output: [1, 1, 0]
+console.log(dailyTemperatures([90, 80, 70, 60])); // Expected output: [0, 0, 0, 0]
+console.log(dailyTemperatures([30, 40, 30, 50, 20, 60])); // Expected output: [1, 2, 1, 1, 1, 0]
